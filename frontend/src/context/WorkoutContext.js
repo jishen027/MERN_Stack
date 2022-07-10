c259b849a0c4a971dd6d4b8fbddf68e3d97378e0
J Li <jli306@sheffield.ac.uk>
2022-07-10 19:37:45 +0100

    [ADD] delete methods

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -5,12 +5,16 @@ export const WorkoutContext = createContext()
 export const workoutsReducer = (state, action) => {
   switch (action.type) {
     case 'SET_WORKOUTS':
-      return { 
-        workouts: action.payload 
+      return {
+        workouts: action.payload
       }
     case 'CREATE_WORKOUT':
-      return { 
-        workouts: [action.payload, ...state.workouts] 
+      return {
+        workouts: [action.payload, ...state.workouts]
+      }
+    case 'DELETE_WORKOUT':
+      return {
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id)
       }
     default:
       return state
@@ -23,7 +27,7 @@ export const WorkoutContextProvider = ({ children }) => {
   })
 
   return (
-    <WorkoutContext.Provider value={{...state, dispatch}}>
+    <WorkoutContext.Provider value={{ ...state, dispatch }}>
       {children}
     </WorkoutContext.Provider>
   )
